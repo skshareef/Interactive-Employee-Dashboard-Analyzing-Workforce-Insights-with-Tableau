b34f2d8b2f2dce33888781d8ab5927aeda6c2802
Shareef Shaik <53653135+skshareef@users.noreply.github.com>
2023-08-03 22:43:44 -0400

    Add files via upload

---
+++ sciprt.sql
@@ -0,0 +1,97 @@
+select emp_no,from_date,to_date
+from t_dept_emp;
+
+
+#Sql code for the first Visualization
+
+	select year(d.from_date) as calender_year,
+	e.gender,
+	count(e.emp_no) as num_of_employyes
+	from t_employees e
+	join
+	t_dept_emp d on d.emp_no = e.emp_no
+	group by calender_year,e.gender
+	having calender_year >=1990;
+    
+    
+    
+    
+#sql code fro second visualization
+USE employees_mod;
+
+
+SELECT 
+    d.dept_name,
+    ee.gender,
+    dm.emp_no,
+    dm.from_date,
+    dm.to_date,
+    e.calendar_year,
+    CASE
+        WHEN YEAR(dm.to_date) >= e.calendar_year AND YEAR(dm.from_date) <= e.calendar_year THEN 1
+        ELSE 0
+    END AS active
+FROM
+    (SELECT 
+        YEAR(hire_date) AS calendar_year
+    FROM
+        t_employees
+    GROUP BY calendar_year) e
+        CROSS JOIN
+    t_dept_manager dm
+        JOIN
+    t_departments d ON dm.dept_no = d.dept_no
+        JOIN 
+    t_employees ee ON dm.emp_no = ee.emp_no
+ORDER BY dm.emp_no, calendar_year;
+    
+    
+    
+    
+#SQL coomand for third Visualization
+
+SELECT 
+    e.gender,
+    d.dept_name,
+    ROUND(AVG(s.salary), 2) AS salary,
+    YEAR(s.from_date) AS calendar_year
+FROM
+    t_salaries s
+        JOIN
+    t_employees e ON s.emp_no = e.emp_no
+        JOIN
+    t_dept_emp de ON de.emp_no = e.emp_no
+        JOIN
+    t_departments d ON d.dept_no = de.dept_no
+GROUP BY d.dept_no , e.gender , calendar_year
+HAVING calendar_year <= 2002
+ORDER BY d.dept_no;
+
+
+
+#SQL command for fourth Visualization
+
+DROP PROCEDURE IF EXISTS filter_salary;
+
+DELIMITER $$
+CREATE PROCEDURE filter_salary (IN p_min_salary FLOAT, IN p_max_salary FLOAT)
+BEGIN
+SELECT 
+    e.gender, d.dept_name, AVG(s.salary) as avg_salary
+FROM
+    t_salaries s
+        JOIN
+    t_employees e ON s.emp_no = e.emp_no
+        JOIN
+    t_dept_emp de ON de.emp_no = e.emp_no
+        JOIN
+    t_departments d ON d.dept_no = de.dept_no
+    WHERE s.salary BETWEEN p_min_salary AND p_max_salary
+GROUP BY d.dept_no, e.gender;
+END$$
+
+DELIMITER ;
+
+CALL filter_salary(50000, 90000);
+    
+    
